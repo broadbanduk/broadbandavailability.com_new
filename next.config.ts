21032b1d70bd3ec59f9146793e85556c555b76e1
Patrick Asare <8025627+patrickAsare@users.noreply.github.com>
2025-11-19 16:31:27 +0000

    Handle static and dynamic deploys

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // ...(process.env.FORGE_DEPLOY !== "true" && { output: "export" }),
-  // reactStrictMode: true,
-  // ...(process.env.FORGE_DEPLOY !== "true" && {
-  //   basePath: "/broadbandavailability.com_new",
-  // }),
-  output: "export",
+  ...(process.env.FORGE_DEPLOY !== "true" && { output: "export" }),
   reactStrictMode: true,
-  basePath: "/broadbandavailability.com_new",
+  ...(process.env.FORGE_DEPLOY !== "true" && {
+    basePath: "/broadbandavailability.com_new",
+  }),
 };
 
 export default nextConfig;
