758bfdfd9b5ddcf7bbd6448cf7b9b17f39e8439a
Max Kobzar <nimbus2219@gmail.com>
2025-11-19 23:51:33 +0000

    mobile version

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ...(process.env.FORGE_DEPLOY !== "true" && { output: "export" }),
+  // ...(process.env.FORGE_DEPLOY !== "true" && { output: "export" }),
+  // reactStrictMode: true,
+  // ...(process.env.FORGE_DEPLOY !== "true" && {
+  //   basePath: "/broadbandavailability.com_new",
+  // }),
+  output: "export",
   reactStrictMode: true,
-  ...(process.env.FORGE_DEPLOY !== "true" && {
-    basePath: "/broadbandavailability.com_new",
-  }),
+  basePath: "/broadbandavailability.com_new",
 };
 
 export default nextConfig;
